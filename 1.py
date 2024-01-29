1751d5a59856c94f5842ff70f611933a376ed1b3
s4kkkk <s4kkkk@mail.ru>
2024-01-29 20:43:12 +0300

    Незначительные правки

--- 1.py
+++ 1.py
@@ -15,6 +15,7 @@ from snake_env import Snake
 
 t.autograd.set_detect_anomaly(True)
 
+
 class SnakeAI(nn.Module):
 
     # структура сети
@@ -26,13 +27,12 @@ class SnakeAI(nn.Module):
         self.inp = nn.Linear(12, 1024)
         self.hidden1 = nn.Linear(1024, 1024)
         self.out = nn.Linear(1024, 4)
-        
+
         # Инициализация слоев
         init.uniform_(self.inp.weight, -1, 1)
         init.uniform_(self.hidden1.weight, -1, 1)
         init.uniform_(self.out.weight, -1, 1)
 
-        
         # Память
         self.memory_states = []
         self.memory_actions = []
@@ -63,9 +63,8 @@ class SnakeAI(nn.Module):
         self.memory_next_states.append(next_state)
         self.memory_isdones.append(1 - isDone)
         self.memory_len += 1
-    
 
-    # метод для получения тензоров 
+    # метод для получения тензоров
     def samplebatch(self):
         states = t.FloatTensor(self.memory_states).cuda()
         self.memory_states.clear()
@@ -78,28 +77,28 @@ class SnakeAI(nn.Module):
 
         next_states = t.FloatTensor(self.memory_next_states).cuda()
         self.memory_next_states.clear()
-        
+
         isdones = t.IntTensor(self.memory_isdones).cuda()
         self.memory_isdones.clear()
-        
+
         self.memory_len = 0
         return (states, actions, rewards, next_states, isdones)
-        
-    
+
     # метод для игры. Первый аргумент - среда, второй - максимальное кол-во эпизодов, третий - максимальное
     # число шагов в одном эпизоде
     def game(self, env, episodes=1000, maxsteps=1000):
         for i in range(episodes):
             state = env.reset()
             for j in range(maxsteps):
-                if(random.random() < self.epsilon):
+                if (random.random() < self.epsilon):
                     action = random.choice(range(4))
                 else:
-                    answer = self.forward((t.FloatTensor(state) * t.FloatTensor([5])).cuda())
+                    answer = self.forward(
+                        (t.FloatTensor(state) * t.FloatTensor([5])).cuda())
                     print(answer)
                     action = t.argmax(answer).item()
                 (next_state, reward, isDone, _) = env.step(action)
-                self.remember(state, action, reward,  next_state, isDone)
+                self.remember(state, action, reward, next_state, isDone)
                 state = next_state
                 if (isDone):
                     break
@@ -114,7 +113,7 @@ class SnakeAI(nn.Module):
         NeuroNextAnswer = self.forward(next_states)
         predicted_now_value = NeuroNowAnswer[range(memory_len), actions]
         predicted_future_value = t.max(NeuroNextAnswer, dim=1)[0]
-        predict_target = rewards + predicted_future_value*isdones
+        predict_target = rewards + predicted_future_value * isdones
         loss = self.criterion(predict_target, predicted_now_value)
         self.loses.append(loss.cpu().item())
         self.rewards_per_epoch.append(t.sum(rewards.cpu()).item())
@@ -124,13 +123,10 @@ class SnakeAI(nn.Module):
             self.inp.weight.grad.data += t.FloatTensor([0.001]).cuda()
         self.optimizer.step()
         print(f"Ошибка: {loss}")
-                    
 
-            
 
 if __name__ == '__main__':
     DEVICE = "cuda"
-
 
     snake = Snake()
     agent = SnakeAI(LearningRate=0.001, Epsilon=0.2).cuda()
@@ -142,13 +138,13 @@ if __name__ == '__main__':
         print(f"Завершено [{i}/{epoch}]")
     print("Тренировка завершена")
     plt.figure(1)
-    plt.plot(epochs,agent.loses)
+    plt.plot(epochs, agent.loses)
     plt.xlabel("Эпохи")
     plt.ylabel("Ошибка")
     plt.grid(True)
 
     plt.figure(2)
-    plt.plot(epochs,agent.rewards_per_epoch)
+    plt.plot(epochs, agent.rewards_per_epoch)
     plt.xlabel("Эпохи")
     plt.ylabel("Награда за эпоху")
     plt.grid(True)
